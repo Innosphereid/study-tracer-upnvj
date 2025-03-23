0736087a745b2ab0dd0193a1417ba57e17507a46
nugrahaAdhitama <2210512109@mahasiswa.upnvj.ac.id>
2025-03-23 21:30:35 +0700

    feat: add PasswordResetSuccessMail class for handling successful password reset notifications

---
+++ app/Mail/PasswordResetSuccessMail.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Mail;
+
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class PasswordResetSuccessMail extends Mailable implements ShouldQueue
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * The user name.
+     *
+     * @var string
+     */
+    public $name;
+
+    /**
+     * The reset time.
+     *
+     * @var \Carbon\Carbon
+     */
+    public $reset_time;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param string $name
+     * @param \Carbon\Carbon $reset_time
+     * @return void
+     */
+    public function __construct(string $name, Carbon $reset_time)
+    {
+        $this->name = $name;
+        $this->reset_time = $reset_time;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Password Berhasil Direset - UPN Veteran Jakarta Tracer Study',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.password.reset-success',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Password Berhasil Direset - UPN Veteran Jakarta Tracer Study')
+                    ->view('emails.password.reset-success')
+                    ->with([
+                        'name' => $this->name,
+                        'reset_time' => $this->reset_time,
+                    ]);
+    }
+}
