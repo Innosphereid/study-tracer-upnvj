9551487eb8b78c197eb4b35a466caf24045c3e40
nugrahaAdhitama <2210512109@mahasiswa.upnvj.ac.id>
2025-03-23 07:09:39 +0700

    feat: enhance alert component to accept custom message and improve styling for alert types

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,23 +1,25 @@
-// resources/views/components/alert.blade.php
-@props(['type' => 'info', 'dismissible' => true])
+@props(['type' => 'info', 'message', 'dismissible' => true])
 
 @php
-$typeClasses = [
-'info' => 'bg-blue-50 text-blue-800 border-blue-200',
+$alertClasses = [
 'success' => 'bg-green-50 text-green-800 border-green-200',
-'warning' => 'bg-yellow-50 text-yellow-800 border-yellow-200',
 'error' => 'bg-red-50 text-red-800 border-red-200',
-][$type] ?? 'bg-blue-50 text-blue-800 border-blue-200';
+'warning' => 'bg-yellow-50 text-yellow-800 border-yellow-200',
+'info' => 'bg-blue-50 text-blue-800 border-blue-200',
+];
 
 $iconClasses = [
-'info' => 'text-blue-400',
 'success' => 'text-green-400',
-'warning' => 'text-yellow-400',
 'error' => 'text-red-400',
-][$type] ?? 'text-blue-400';
+'warning' => 'text-yellow-400',
+'info' => 'text-blue-400',
+];
+
+$alertClass = $alertClasses[$type] ?? $alertClasses['info'];
+$iconClass = $iconClasses[$type] ?? $iconClasses['info'];
 @endphp
 
-<div {{ $attributes->merge(['class' => "border rounded-lg p-4 mb-4 {$typeClasses}"]) }} x-data="{ show: true }"
+<div {{ $attributes->merge(['class' => "border rounded-lg p-4 mb-4 {$alertClass}"]) }} x-data="{ show: true }"
     x-show="show" x-transition:enter="transition ease-out duration-300"
     x-transition:enter-start="opacity-0 transform scale-95" x-transition:enter-end="opacity-100 transform scale-100"
     x-transition:leave="transition ease-in duration-200" x-transition:leave-start="opacity-100 transform scale-100"
@@ -25,39 +27,39 @@ $iconClasses = [
 
     <div class="flex items-start">
         <div class="flex-shrink-0">
-            @if ($type === 'info')
-            <svg class="h-5 w-5 {{ $iconClasses }}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-                fill="currentColor">
-                <path fill-rule="evenodd"
-                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
-                    clip-rule="evenodd" />
-            </svg>
-            @elseif ($type === 'success')
-            <svg class="h-5 w-5 {{ $iconClasses }}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+            @if ($type === 'success')
+            <svg class="h-5 w-5 {{ $iconClass }}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
                 fill="currentColor">
                 <path fill-rule="evenodd"
                     d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
                     clip-rule="evenodd" />
             </svg>
+            @elseif ($type === 'error')
+            <svg class="h-5 w-5 {{ $iconClass }}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                fill="currentColor">
+                <path fill-rule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
+                    clip-rule="evenodd" />
+            </svg>
             @elseif ($type === 'warning')
-            <svg class="h-5 w-5 {{ $iconClasses }}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+            <svg class="h-5 w-5 {{ $iconClass }}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
                 fill="currentColor">
                 <path fill-rule="evenodd"
                     d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z"
                     clip-rule="evenodd" />
             </svg>
-            @elseif ($type === 'error')
-            <svg class="h-5 w-5 {{ $iconClasses }}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+            @else
+            <svg class="h-5 w-5 {{ $iconClass }}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
                 fill="currentColor">
                 <path fill-rule="evenodd"
-                    d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
+                    d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
                     clip-rule="evenodd" />
             </svg>
             @endif
         </div>
         <div class="ml-3 flex-1">
             <div class="text-sm font-medium">
-                {{ $slot }}
+                {{ $message }}
             </div>
         </div>
 
@@ -65,7 +67,7 @@ $iconClasses = [
         <div class="ml-auto pl-3">
             <div class="-mx-1.5 -my-1.5">
                 <button @click="show = false" type="button"
-                    class="{{ $iconClasses }} rounded-md p-1.5 inline-flex hover:bg-opacity-10 hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-transparent transition-colors duration-200">
+                    class="{{ $iconClass }} rounded-md p-1.5 inline-flex hover:bg-opacity-10 hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-transparent transition-colors duration-200">
                     <span class="sr-only">Dismiss</span>
                     <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                         <path fill-rule="evenodd"
