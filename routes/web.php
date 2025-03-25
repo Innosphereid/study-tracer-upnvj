c8d66a66b930644125c7aed2f3f0d38d6adc20ce
nugrahaAdhitama <2210512109@mahasiswa.upnvj.ac.id>
2025-03-25 15:29:24 +0700

    feat: add questionnaire routes for admin and public access

--- routes/web.php
+++ routes/web.php
@@ -112,3 +112,40 @@ Route::prefix('email-preview')->name('email.preview.')->group(function () {
         ]);
     })->name('reset-success');
 });
+
+// Questionnaire Routes - Accessible by admin and superadmin
+Route::middleware(['auth', 'role:admin,superadmin'])->group(function () {
+    // Questionnaire CRUD
+    Route::get('/questionnaires', [QuestionnaireController::class, 'index'])->name('questionnaires.index');
+    Route::get('/questionnaires/create', [QuestionnaireController::class, 'create'])->name('questionnaires.create');
+    Route::post('/questionnaires', [QuestionnaireController::class, 'store'])->name('questionnaires.store');
+    Route::get('/questionnaires/{id}/edit', [QuestionnaireController::class, 'edit'])->name('questionnaires.edit');
+    Route::put('/questionnaires/{id}', [QuestionnaireController::class, 'update'])->name('questionnaires.update');
+    Route::delete('/questionnaires/{id}', [QuestionnaireController::class, 'destroy'])->name('questionnaires.destroy');
+    
+    // Additional questionnaire actions
+    Route::get('/questionnaires/{id}/preview', [QuestionnaireController::class, 'preview'])->name('questionnaires.preview');
+    Route::post('/questionnaires/{id}/publish', [QuestionnaireController::class, 'publish'])->name('questionnaires.publish');
+    Route::post('/questionnaires/{id}/close', [QuestionnaireController::class, 'close'])->name('questionnaires.close');
+    Route::post('/questionnaires/{id}/clone', [QuestionnaireController::class, 'clone'])->name('questionnaires.clone');
+    Route::post('/questionnaires/{id}/generate-link', [QuestionnaireController::class, 'generateLink'])->name('questionnaires.generate-link');
+    Route::get('/questionnaires/{id}/statistics', [QuestionnaireController::class, 'statistics'])->name('questionnaires.statistics');
+    
+    // Section API routes
+    Route::post('/sections', [SectionController::class, 'store'])->name('sections.store');
+    Route::put('/sections/{id}', [SectionController::class, 'update'])->name('sections.update');
+    Route::delete('/sections/{id}', [SectionController::class, 'destroy'])->name('sections.destroy');
+    Route::post('/sections/reorder', [SectionController::class, 'reorder'])->name('sections.reorder');
+    
+    // Question API routes
+    Route::post('/questions', [QuestionController::class, 'store'])->name('questions.store');
+    Route::put('/questions/{id}', [QuestionController::class, 'update'])->name('questions.update');
+    Route::delete('/questions/{id}', [QuestionController::class, 'destroy'])->name('questions.destroy');
+    Route::post('/questions/reorder', [QuestionController::class, 'reorder'])->name('questions.reorder');
+});
+
+// Public questionnaire routes - For respondents (alumni) - No authentication required
+Route::get('/kuesioner/{code}', [QuestionnaireController::class, 'showPublic'])->name('questionnaires.public.show');
+Route::post('/kuesioner/{code}/start-response', [QuestionnaireController::class, 'startResponse'])->name('questionnaires.public.start-response');
+Route::post('/kuesioner/submit-answer', [QuestionnaireController::class, 'submitAnswer'])->name('questionnaires.public.submit-answer');
+Route::post('/kuesioner/complete-response', [QuestionnaireController::class, 'completeResponse'])->name('questionnaires.public.complete-response');
