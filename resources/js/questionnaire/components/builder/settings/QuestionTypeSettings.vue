b337472db6e5c9d39ff5d37da42b1ed2bfb295cf
nugrahaAdhitama <2210512109@mahasiswa.upnvj.ac.id>
2025-03-27 06:19:10 +0700

    feat: refactor QuestionTypeSettings to conditionally render question type components, enhancing configurability and user experience in the questionnaire builder

--- resources/js/questionnaire/components/builder/settings/QuestionTypeSettings.vue
+++ resources/js/questionnaire/components/builder/settings/QuestionTypeSettings.vue
@@ -1,19 +1,89 @@
 <template>
-    <component
-        :is="questionTypeComponent"
-        v-if="questionTypeComponent"
-        :question="question"
-        @update:question="$emit('update:question', $event)"
-        @duplicate-question="$emit('duplicate-question')"
-        @delete-question="$emit('delete-question')"
-    />
-    <div v-else class="py-6 text-center text-gray-500">
-        Loading question settings...
+    <!-- Menggunakan pendekatan v-if untuk menampilkan komponen secara konditional -->
+    <div>
+        <!-- Komponen sesuai tipe pertanyaan -->
+        <RadioQuestionSettings
+            v-if="question.type === 'radio'"
+            :question="question"
+            @update:question="$emit('update:question', $event)"
+            @duplicate-question="$emit('duplicate-question')"
+            @delete-question="$emit('delete-question')"
+        />
+
+        <CheckboxQuestionSettings
+            v-else-if="question.type === 'checkbox'"
+            :question="question"
+            @update:question="$emit('update:question', $event)"
+            @duplicate-question="$emit('duplicate-question')"
+            @delete-question="$emit('delete-question')"
+        />
+
+        <DropdownQuestionSettings
+            v-else-if="question.type === 'dropdown'"
+            :question="question"
+            @update:question="$emit('update:question', $event)"
+            @duplicate-question="$emit('duplicate-question')"
+            @delete-question="$emit('delete-question')"
+        />
+
+        <RatingQuestionSettings
+            v-else-if="question.type === 'rating'"
+            :question="question"
+            @update:question="$emit('update:question', $event)"
+            @duplicate-question="$emit('duplicate-question')"
+            @delete-question="$emit('delete-question')"
+        />
+
+        <LikertQuestionSettings
+            v-else-if="question.type === 'likert'"
+            :question="question"
+            @update:question="$emit('update:question', $event)"
+            @duplicate-question="$emit('duplicate-question')"
+            @delete-question="$emit('delete-question')"
+        />
+
+        <FileUploadQuestionSettings
+            v-else-if="question.type === 'file-upload'"
+            :question="question"
+            @update:question="$emit('update:question', $event)"
+            @duplicate-question="$emit('duplicate-question')"
+            @delete-question="$emit('delete-question')"
+        />
+
+        <MatrixQuestionSettings
+            v-else-if="question.type === 'matrix'"
+            :question="question"
+            @update:question="$emit('update:question', $event)"
+            @duplicate-question="$emit('duplicate-question')"
+            @delete-question="$emit('delete-question')"
+        />
+
+        <RankingQuestionSettings
+            v-else-if="question.type === 'ranking'"
+            :question="question"
+            @update:question="$emit('update:question', $event)"
+            @duplicate-question="$emit('duplicate-question')"
+            @delete-question="$emit('delete-question')"
+        />
+
+        <QuestionSettingsPanel
+            v-else
+            :question="question"
+            @update:question="$emit('update:question', $event)"
+            @duplicate-question="$emit('duplicate-question')"
+            @delete-question="$emit('delete-question')"
+        >
+            <template #type-specific-settings>
+                <div class="py-4 text-center text-sm text-gray-500">
+                    Tidak ada pengaturan khusus untuk tipe {{ question.type }}
+                </div>
+            </template>
+        </QuestionSettingsPanel>
     </div>
 </template>
 
 <script setup>
-import { defineProps, defineEmits, computed } from "vue";
+import { defineProps, defineEmits, onMounted } from "vue";
 import RadioQuestionSettings from "./RadioQuestionSettings.vue";
 import CheckboxQuestionSettings from "./CheckboxQuestionSettings.vue";
 import DropdownQuestionSettings from "./DropdownQuestionSettings.vue";
@@ -21,6 +91,7 @@ import RatingQuestionSettings from "./RatingQuestionSettings.vue";
 import LikertQuestionSettings from "./LikertQuestionSettings.vue";
 import FileUploadQuestionSettings from "./FileUploadQuestionSettings.vue";
 import MatrixQuestionSettings from "./MatrixQuestionSettings.vue";
+import RankingQuestionSettings from "./RankingQuestionSettings.vue";
 import QuestionSettingsPanel from "./QuestionSettingsPanel.vue";
 
 const props = defineProps({
@@ -36,27 +107,9 @@ const emit = defineEmits([
     "delete-question",
 ]);
 
-// Map question types to their respective components
-const questionTypeComponent = computed(() => {
-    // Implement specialized components
-    if (props.question.type === "radio") {
-        return RadioQuestionSettings;
-    } else if (props.question.type === "checkbox") {
-        return CheckboxQuestionSettings;
-    } else if (props.question.type === "dropdown") {
-        return DropdownQuestionSettings;
-    } else if (props.question.type === "rating") {
-        return RatingQuestionSettings;
-    } else if (props.question.type === "likert") {
-        return LikertQuestionSettings;
-    } else if (props.question.type === "file-upload") {
-        return FileUploadQuestionSettings;
-    } else if (props.question.type === "matrix") {
-        return MatrixQuestionSettings;
-    }
-
-    // Default to basic settings panel for other question types
-    // Later we'll add more specialized components for other question types
-    return QuestionSettingsPanel;
+// Debug initial question
+onMounted(() => {
+    console.log("QuestionTypeSettings mounted with question:", props.question);
+    console.log("Question type:", props.question.type);
 });
 </script>
