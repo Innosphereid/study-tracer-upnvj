e82a9d274ab99ad5451e1abb0a5669d1acd60422
nugrahaAdhitama <2210512109@mahasiswa.upnvj.ac.id>
2025-03-26 23:19:14 +0700

    feat: add BaseQuestionPreview component for rendering question previews with slot support

---
+++ resources/js/questionnaire/components/builder/preview/BaseQuestionPreview.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="question-preview">
+        <slot></slot>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    question: {
+        type: Object,
+        required: true,
+    },
+});
+</script>
