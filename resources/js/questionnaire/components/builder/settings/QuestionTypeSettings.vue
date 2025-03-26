f5814a54f33a70f283139a60d73c6c18f85e4967
nugrahaAdhitama <2210512109@mahasiswa.upnvj.ac.id>
2025-03-27 00:14:00 +0700

    feat: create QuestionTypeSettings component to dynamically render question settings based on question type, supporting radio and checkbox configurations

---
+++ resources/js/questionnaire/components/builder/settings/QuestionTypeSettings.vue
@@ -0,0 +1,47 @@
+<template>
+    <component
+        :is="questionTypeComponent"
+        v-if="questionTypeComponent"
+        :question="question"
+        @update:question="$emit('update:question', $event)"
+        @duplicate-question="$emit('duplicate-question')"
+        @delete-question="$emit('delete-question')"
+    />
+    <div v-else class="py-6 text-center text-gray-500">
+        Loading question settings...
+    </div>
+</template>
+
+<script setup>
+import { defineProps, defineEmits, computed } from "vue";
+import RadioQuestionSettings from "./RadioQuestionSettings.vue";
+import CheckboxQuestionSettings from "./CheckboxQuestionSettings.vue";
+import QuestionSettingsPanel from "./QuestionSettingsPanel.vue";
+
+const props = defineProps({
+    question: {
+        type: Object,
+        required: true,
+    },
+});
+
+const emit = defineEmits([
+    "update:question",
+    "duplicate-question",
+    "delete-question",
+]);
+
+// Map question types to their respective components
+const questionTypeComponent = computed(() => {
+    // Implement specialized components
+    if (props.question.type === "radio") {
+        return RadioQuestionSettings;
+    } else if (props.question.type === "checkbox") {
+        return CheckboxQuestionSettings;
+    }
+
+    // Default to basic settings panel for other question types
+    // Later we'll add more specialized components for other question types
+    return QuestionSettingsPanel;
+});
+</script>
