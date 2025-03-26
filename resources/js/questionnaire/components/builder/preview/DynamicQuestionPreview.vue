6646d59ae1a321afd4d778e39c6a71a5aabbfe06
nugrahaAdhitama <2210512109@mahasiswa.upnvj.ac.id>
2025-03-26 23:19:41 +0700

    feat: add DynamicQuestionPreview component for rendering question previews dynamically based on question type

---
+++ resources/js/questionnaire/components/builder/preview/DynamicQuestionPreview.vue
@@ -0,0 +1,30 @@
+<template>
+    <component
+        v-if="previewComponent"
+        :is="previewComponent"
+        :question="question"
+    />
+    <div
+        v-else
+        class="mt-1 py-2 px-3 text-sm text-gray-500 bg-gray-50 border border-gray-200 rounded-md"
+    >
+        [Preview untuk tipe pertanyaan {{ question.type }}]
+    </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import { getPreviewComponent } from "./QuestionPreviewRegistry";
+
+const props = defineProps({
+    question: {
+        type: Object,
+        required: true,
+    },
+});
+
+// Mendapatkan komponen yang sesuai dengan tipe pertanyaan
+const previewComponent = computed(() => {
+    return getPreviewComponent(props.question.type);
+});
+</script>
