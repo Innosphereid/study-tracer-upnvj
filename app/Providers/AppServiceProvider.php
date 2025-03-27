d1de45cabab4c9a0c2e0aba8073c2fee0522f0e5
nugrahaAdhitama <2210512109@mahasiswa.upnvj.ac.id>
2025-03-27 07:52:51 +0700

    feat: enforce HTTPS in production and set default string length for schema

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +25,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Enforce HTTPS in production
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+        
+        // Set default string length for schema
         Schema::defaultStringLength(191);
     }
 }
